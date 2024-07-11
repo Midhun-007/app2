54ab54e359b1cc05eca34935142f98f3b3d089a9
Midhun.S <midhun1012@gmail.com>
2024-07-11 12:09:42 +0530

    ff frnds

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ import pandas as p
 s.set_page_config(layout='wide')
 s.title('Free Fire Friends')
 col1,col2,col3=s.columns([1.5,0.5,1.5])
-a=p.read_csv(r'C:\Users\Midhun.S\PycharmProjects\app-2\selfcoding\ff.csv')
+a=p.read_csv('ff.csv')
 with col1:
     for index,row in a[:4].iterrows():
         s.header(row['name'])
